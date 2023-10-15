9d9141eafde3cc17374c0abd4ee73c2d2a9b2fe2
Sam Poder <sam.r.poder@gmail.com>
2023-10-14 18:25:37 -0700

    Add my Vercel proxy thing

---
+++ api/[...path].js
@@ -0,0 +1,25 @@
+const S1 = require('s1db')
+const https = require('https');
+const db = new S1(process.env.S1_TOKEN)
+import httpProxy from 'http-proxy'
+
+const proxy = httpProxy.createProxyServer()
+
+// Make sure that we don't parse JSON bodies on this route:
+export const config = {
+	api: {
+		bodyParser: false,
+	},
+}
+
+export default async (req, res) => {
+	let url = await db.get(req.hostname.split(".")[0])
+	return new Promise((resolve, reject) => {
+		proxy.web(req, res, { target: url, changeOrigin: true }, (err) => {
+			if (err) {
+				return reject(err)
+			}
+			resolve()
+		})
+	})
+}
